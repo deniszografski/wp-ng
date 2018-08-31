89672731a5e8ce5cf8be25411cd5644062a618c7
Denis Zografski <zografski.denis@gmail.com>
2018-08-31 16:44:18 +0200

    HTML5 theme
    Signed-off-by: Denis Zografski <zografski.denis@gmail.com>

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'app'
 }
